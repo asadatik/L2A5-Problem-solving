e07e62418950e3e3ec128cb65e0f1c257248407b
Asadujjaman Atik <155472906+asadatik@users.noreply.github.com>
2025-05-08 23:20:43 +0600

    Added  the  solution of last problem

--- src/index.ts
+++ src/index.ts
@@ -121,4 +121,14 @@ function getDayType(day: Day): string {
 }
 
 
-
+async function squareAsync(n: number): Promise<number> {
+  return new Promise((resolve, reject) => {
+    if (n < 0) {
+      reject(new Error("Negative number not allowed"));
+    } else {
+      setTimeout(() => {
+        resolve(n * n);
+      }, 1000);
+    }
+  });
+}
